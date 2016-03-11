5762b0afb0f3c7e95d0dda0dafd605d1323abf07
Jonathan Hollinger <jonathanhollinger@gmail.com>
2016-03-11 15:29:45 -0700

    Bug fixes

--- test/index.js
+++ test/index.js
@@ -62,6 +62,96 @@ describe('Module', function() {
         expect(schema.tables.test).to.be.ok;
     });
     
+    it("can insert a record", function(done) {
+        schema.tables.test.insert({ id: "1", value: "one" }, function(err) {
+            if (err) throw err;
+            done();
+        });
+    });
+    
+    it("cannot re-insert a record", function(done) {
+        schema.tables.test.insert({ id: "1", value: "one" }, function(err) {
+            err.should.be.ok;
+            done();
+        });
+    });
+    
+    it("can overwrite a record", function(done) {
+        schema.tables.test.overwrite({ id: "1", value: "two" }, function(err) {
+            if (err) throw err;
+            done();
+        });
+    });
+    
+    it("can confirm a record exists", function(done) {
+        schema.tables.test.exists({ id: "1" }, function(err, exists) {
+            if (err) throw err;
+            else expect(exists).to.be.ok;
+            done();
+        });
+    });
+    
+    it("can get a record", function(done) {
+        schema.tables.test.get({ id: "1" }, function(err, item) {
+            if (err) throw err;
+            else expect(item).to.be.an("object");
+            done();
+        });
+    });
+    
+    it("can get part of a record", function(done) {
+        schema.tables.test.getPart({ id: "1" }, [ "id" ], function(err, item) {
+            if (err) throw err;
+            else {
+                expect(item).to.be.an("object");
+                Object.keys(item).length.should.equal(1);
+            }
+            done();
+        });
+    });
+    
+    it("cannot conditionally delete a record with incorrect values", function(done) {
+        schema.tables.test.deleteIf({ id: "1" }, { value: "one" }, function(err, item) {
+            err.should.be.ok;
+            done();
+        });
+    });
+    
+    it("can conditionally delete a record with correct values", function(done) {
+        schema.tables.test.deleteIf({ id: "1" }, { value: "two" }, function(err) {
+            if (err) throw err;
+            done();
+        });
+    });
+    
+    it("can delete a record twice", function(done) {
+        schema.tables.test.delete({ id: "1" }, function(err) {
+            if (err) throw err;
+            done();
+        });
+    });
+    
+    it("can write a record", function(done) {
+        schema.tables.test.write({ id: "1", value: "one" }).insert(function(err) {
+            if (err) throw err;
+            done();
+        });
+    });
+    
+    it("can delete a record", function(done) {
+        schema.tables.test.delete({ id: "1" }, function(err) {
+            done();
+        });
+    });
+    
+    it("can confirm a record does not exists", function(done) {
+        schema.tables.test.exists({ id: "1" }, function(err, exists) {
+            if (err) throw err;
+            else expect(exists).to.be.not.ok;
+            done();
+        });
+    });
+    
     it("can drop a schema", function(done) {
         this.timeout(120000);
         schema.drop(function(err) {
